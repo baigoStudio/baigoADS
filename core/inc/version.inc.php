307cd10701a4dcd152ca9aeb457e29267719089f
fone <fone.huangyf@gmail.com>
2018-08-17 10:38:49 +0800

    v2.1

--- core/inc/version.inc.php
+++ core/inc/version.inc.php
@@ -11,7 +11,7 @@ if (!defined('IN_BAIGO')) {
 define('PRD_ADS_POWERED', 'Powered by');
 define('PRD_ADS_NAME', 'baigo ADS');
 define('PRD_ADS_URL', 'http://www.baigo.net/ads/');
-define('PRD_ADS_VER', '2.0');
-define('PRD_ADS_PUB', 20171211);
+define('PRD_ADS_VER', '2.1');
+define('PRD_ADS_PUB', 20180817);
 define('PRD_VER_CHECK', 'http://www.baigo.net/ver_check/check.php');
 
